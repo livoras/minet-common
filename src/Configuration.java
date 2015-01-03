3779d6321e96a1a8c6b0dc5fc05443f328f6504c
livoras <livoras@163.com>
2015-01-03 12:23:53 +0800

    Using configuration file instead of hard code

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,6 +1,32 @@
 package Common.src;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Configuration {
-	public static final String HOST = "127.0.0.1";
-	public static final int PORT = 19931;
+	public String HOST; // 部署的时候，这里需要换成本地的IP地址
+	public int PORT;
+
+	public Configuration() {
+	    String path = Paths.get(".").toAbsolutePath().normalize().toString();
+	    try {
+            @SuppressWarnings("resource")
+            String content = new Scanner(new File(path + "/" + "config.json")).useDelimiter("\\Z").next();
+            JSONObject obj = new JSONObject(content);
+            HOST = obj.getString("HOST");
+            PORT = obj.getInt("PORT");
+        } catch (FileNotFoundException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+	public static void main(String[] args) {
+        Configuration config = new Configuration();
+        System.out.println(config.HOST + ":" + config.PORT);
+    }
 }
